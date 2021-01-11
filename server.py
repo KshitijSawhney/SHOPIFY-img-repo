aee23f14c36bb922719b8039699bc849733f47c0
Kshitij Sawhney <engg.kshitij@gmail.com>
2021-01-12 02:03:36 +0530

    added simple log in logic

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #Shopify image repo
-from flask import Flask,render_template
+from flask import Flask, render_template, redirect, url_for, request
 import sqlite3 as sql
 
+logged_in = False
 app = Flask(__name__)
 
 def get_cursor():
@@ -15,6 +16,10 @@ def initialize():
     #populate the database with basic info
 
     #users
+    """
+    contains user login info to conditionally render different versions of the webpage 
+    for different users.
+    """
     cur.execute("DROP TABLE IF EXISTS users")
     cur.execute("CREATE TABLE users (ID TEXT, password TEXT, vendor BOOLEAN)")
     cur.execute("INSERT INTO users (ID, password, vendor) VALUES\
@@ -32,3 +37,32 @@ def initialize():
     #transactions
     cur.execute("DROP TABLE IF EXISTS transactions")
     cur.execute("CREATE TABLE transactions (name TEXT, amount FLOAT, userID TEXT)")
+
+    conn.commit()
+    print("Database created")
+
+@app.route("/") #homepage
+def home_page():
+    if not logged_in:
+        return render_template("info.html",info="please log-in first")
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    (cur, conn) = get_cursor()
+    error = None
+    if request.method == 'POST':
+        cur.execute("SELECT * FROM users WHERE (ID=%s AND password=%s)"%(request.form['username'],request.form['password']))
+        result = cur.fetchhone()
+        
+        if not result:
+            return render_template("info.html",info="incorrect login")
+        (_,_,vendor)=result
+
+        if vendor:
+            return render_template("vendor.html")
+        if not vendor:
+            return render_template("index.html")
+        else:
+            return render_template("info.html",info="something went wrong")
+    return render_template('login.html', error=error)
